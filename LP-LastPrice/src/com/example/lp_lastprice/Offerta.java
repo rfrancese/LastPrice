ef84f705c5703f29071ae4f34ff700395abba2d0
Vincenzo <v.vairo1@libero.it>
2014-04-30 12:21:39 +0200

    ver 0.4
    Signed-off-by: Vincenzo <v.vairo1@libero.it>

--- LP-LastPrice/src/com/example/lp_lastprice/Offerta.java
+++ LP-LastPrice/src/com/example/lp_lastprice/Offerta.java
@@ -1,64 +1,27 @@
 package com.example.lp_lastprice;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
 import android.app.ListActivity;
-public class Offerta extends Activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_offerta);
+public class Offerta extends ListActivity {
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    String[] values = new String[] { "Vacanze", "Benessere", "Sport",
+        "Svago", "Ristoranti","Tecnologia", "LP-LastPrice" };
+    // use your custom layout
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        R.layout.fragment_offerta, R.id.label, values);
+    setListAdapter(adapter);
+  }
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.offerta, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_offerta,
-					container, false);
-			return rootView;
-		}
-	}
-
-}
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    String item = (String) getListAdapter().getItem(position);
+    Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+  }
+} 
